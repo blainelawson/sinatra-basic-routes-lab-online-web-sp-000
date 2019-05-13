dd51ecf0c86584b309813315e1ff89bd5b970f21
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-13 14:44:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,4 +7,12 @@ class App < Sinatra::Base
     binding.pry
     "My name is __"
   end
+
+  get '/hometown' do
+    "My hometown is ___"
+  end
+
+  get '/fovorite-song'
+    "My favorite song is ___"
+  end
 end
